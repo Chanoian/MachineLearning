d2d038ea5db9a108ce0e5e7d24d61046e3a9f2b2
AraC <shanoian90@gmail.com>
2019-05-12 12:55:14 -0400

    First Commit

---
+++ farenhietT0Celesius.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+import tensorflow as tf 
+
+celsius_q    = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
+fahrenheit_a = np.array([-40,  14, 32, 46, 59, 72, 100],  dtype=float)
+
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1]) 
+model = tf.keras.Sequential([l0])
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+history = model.fit(celsius_q, fahrenheit_a, epochs=1500, verbose=False)
+print("Finished training the model")
+print(model.predict([100.0]))
---
+++ firstClassifier.py
@@ -0,0 +1,43 @@
+from sklearn import datasets
+from sklearn.metrics import accuracy_score
+from scipy.spatial import distance
+from sklearn.model_selection import train_test_split
+
+def euc(a,b):
+    return distance.euclidean(a,b)
+
+class ScrapyKNN():
+    def fit(self, X_train, Y_train ):
+        self.X_train = X_train
+        self.Y_train = Y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = self.closest(row)
+            predictions.append(label)
+        return predictions
+    
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            dist = euc(row, self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.Y_train[best_index]
+
+
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=.5)
+my_classifier = ScrapyKNN()
+my_classifier.fit(X_train, Y_train)
+predictions = my_classifier.predict(X_test)
+
+print (accuracy_score(Y_test, predictions))
